ff18fb3d0682b3cf782112c2136bef76265801bc
tduran <Trevor_Duran@intuit.com>
2022-03-28 19:20:28 -0600

    - Adds dynamic extension parsing - Replaces semicolon in file path components - Adds logging for embedded cover art metadata - Adds collecting stats for metadata tag sources

--- index.mjs
+++ index.mjs
@@ -3,14 +3,16 @@ import glob from 'glob-promise';
 import log from 'npmlog';
 import { copyFile, stat, mkdir } from 'fs/promises';
 import { createWriteStream } from 'fs';
-import { extname, join } from 'path';
+import { basename, extname, join } from 'path';
 
-const inputDir = join(process.env.HOME, 'Downloads', 'Music');
+const inputDir = join(process.env.HOME, 'Plex', 'Music');
+const lostDir = join(process.env.HOME, 'Plex', '?');
 const outputDir = join(process.env.HOME, 'Downloads', 'output');
 const extensions = ['mp3', 'm4a'];
 
 const logPrefix = 'RE-MIXER';
 const mkdirOpts = { recursive: true};
+const metadataTagTypes = { None: 0 };
 
 const parseFiles = async (audioFiles) => {
     let numSuccesses = 0;
@@ -19,20 +21,41 @@ const parseFiles = async (audioFiles) => {
         let metadata;
         try {
             metadata = await mm.parseFile(audioFile);
+            if (metadata.format.tagTypes.length === 0) {
+                metadataTagTypes['None']++;
+                log.warn(logPrefix, '\tNo metadata tags %s', audioFile);
+                const audioFileBasename = basename(audioFile);
+                await copyFile(audioFile, `${lostDir}/${audioFileBasename}`);
+                continue;
+            }
+            metadata.format.tagTypes.forEach(tagType => {
+                if (!metadataTagTypes[tagType]) {
+                    metadataTagTypes[tagType] = 1;
+                }
+                else {
+                    metadataTagTypes[tagType]++;
+                }
+            });
         }
         catch (error) {
             log.error(logPrefix, 'Error occurred while parsing audio file %s! %s', audioFile, error.message);
         }
-        let { artist, album, title, track } = metadata.common;
-        if (!isValidSongMetadata(artist, album, title, track)) {
+        let { artist, album, albumartist, title, track, picture } = metadata.common;
+        if (!isValidSongMetadata(artist, album, albumartist, title, track, picture)) {
+            const audioFileBasename = basename(audioFile);
+            await copyFile(audioFile, `${lostDir}/${audioFileBasename}`);
             continue;
         }
         artist = sanitizeFilenameComponent(artist);
         album = sanitizeFilenameComponent(album);
+        if (albumartist) {
+            albumartist = sanitizeFilenameComponent(albumartist);
+        }
         title = sanitizeFilenameComponent(title);
         track = standardizeFilenameTrack(track);
-        const songFilePathName = getSongFilePathName(audioFile, artist, album, title, track);
-        await createArtistAlbumPath(artist, album);
+        const albumArtist = albumartist ? albumartist : album;
+        const songFilePathName = getSongFilePathName(audioFile, albumArtist, album, title, track);
+        await createArtistAlbumPath(albumArtist, album);
         await copyFile(audioFile, songFilePathName);
         log.info(logPrefix, '\t%s >>> %s', audioFile, songFilePathName);
         numSuccesses++;
@@ -40,10 +63,12 @@ const parseFiles = async (audioFiles) => {
     return numSuccesses;
 };
 
-const isValidSongMetadata = (artist, album, title, track) => {
+const isValidSongMetadata = (artist, album, albumArtist, title, track, picture) => {
     const artistExists = verifyMetadataComponentExists('Artist', artist);
     const albumExists = verifyMetadataComponentExists('Album', album);
+    verifyMetadataComponentExists('Album Artist', albumArtist);
     const titleExists = verifyMetadataComponentExists('Title', title);
+    verifyMetadataComponentExists('Picture', picture);
     const trackNoExists = verifyMetadataComponentExists('Track number', track.no);
     const trackOfExists = verifyMetadataComponentExists('Track number out of', track.of);
 
@@ -58,6 +83,8 @@ const verifyMetadataComponentExists = (datumName, datum) => {
 }
 
 const sanitizeFilenameComponent = (fileName) => fileName
+    .replaceAll('; ', '﹔')
+    .replaceAll(';', '﹔')
     .replaceAll(': ', '：')
     .replaceAll(':', '﹕')
     .replaceAll('/', '∕')
@@ -68,9 +95,9 @@ const standardizeFilenameTrack = (track) => {
     return track.no.toString().padStart(paddingLength, '0');
 };
 
-const getSongFilePathName = (audioFile, artist, album, title, track) => {
+const getSongFilePathName = (audioFile, albumArtist, album, title, track) => {
     const extension = extname(audioFile);
-    const songFilePath = join(outputDir, artist, album);
+    const songFilePath = join(outputDir, albumArtist, album);
     return `${songFilePath}/${track} - ${title}${extension}`;
 };
 
@@ -99,9 +126,11 @@ if (!await directoryExists('logs')) {
 log.stream = createWriteStream(`logs/${(new Date()).toISOString()}.log`);
 
 log.info(logPrefix, 'Recursively scanning for %s files found within input path %s', extensions.toString(), inputDir);
-const filesToParse = await glob(`${inputDir}/**/*.{${extensions.toString()}}`);
+const globExtensionComponent = extensions.length === 1 ? extensions[0] : `{${extensions.toString()}}`;
+const filesToParse = await glob(`${inputDir}/**/*.${globExtensionComponent}`);
 log.info(logPrefix, 'Found %d audio files (%s) to process within %s', filesToParse.length, extensions.toString(), inputDir);
 const numSuccessfullyProcessed = await parseFiles(filesToParse);
 log.info(logPrefix, 'Processed files: %d', filesToParse.length);
 log.info(logPrefix, 'Automatically organized files: %d', numSuccessfullyProcessed);
 log.info(logPrefix, 'Files needing manual intervention: %d', filesToParse.length - numSuccessfullyProcessed);
+log.info(logPrefix, 'Metadata tag types encountered (multiples can occur)', metadataTagTypes);
